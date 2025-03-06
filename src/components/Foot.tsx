1f7473e64a4af3908a6c0e69bd655795e2022cdb
aaronhuan <aaronhuang0000@gmail.com>
2025-03-05 20:32:17 -0500

    change a to Link

--- src/components/Foot.tsx
+++ src/components/Foot.tsx
@@ -1,12 +1,11 @@
 import styled from "styled-components";
-//     <link rel="shortcut icon" href="#">
-
+import { Link } from "react-router-dom"
 const StyleP = styled.p `
 font-family: Georgia, 'Times New Roman', Times, serif;
 `;
 
 export default function Foot(){
     return(
-        <StyleP>All rights reserved by Aaron Huang <a href ="">Credits</a> &#169; </StyleP>
+        <StyleP>All rights reserved by Aaron Huang <Link to="">Credits</Link> &#169; </StyleP>
     ); 
 }
